72be7ad21f4aacc96f90d0b26c6ae71c2cca84eb
waqas Alam <alamwaqas@gmail.com>
2017-02-20 15:31:53 -0800

    make server multi-threaded

--- server.go
+++ server.go
@@ -16,12 +16,33 @@ const (
 	//CONN_TYPE = "tcp"
 )
 
+type Message struct {
+	conn net.Conn
+	name string
+}
+
+func handler(c chan *Message, done chan struct{}) {
+
+	for msg := range c {
+		msg := msg
+		fmt.Printf("message rcvd %s on", msg.name)
+		msg.conn.Write([]byte("Message received." + msg.name))
+		msg.conn.Close()
+		done <- struct{}{}
+	}
+}
+
 func main() {
 	//origns, err := GetFromThread()
 	//if err != nil {
 	//	t.Fatal(err)
 	//}
+	fmt.Println("Main program started", unix.Gettid)
+
 	done := make(chan struct{})
+	msgc := make(chan *Message)
+	go handler(msgc, done)
+
 	nsfunc := func(path string) {
 		runtime.LockOSThread()
 		fmt.Println(path)
@@ -39,33 +60,38 @@ func main() {
 			fmt.Println("Error listening:", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("Listening on "+":"+CONN_PORT, unix.Gettid)
-		for i := 0; i < 2; i++ {
-			// Listen for an incoming connection.
-			conn, err := l.Accept()
-			if err != nil {
-				fmt.Println("Error accepting: ", err.Error())
-				os.Exit(1)
-			}
 
-			buf := make([]byte, 1024)
-			_, err = conn.Read(buf)
-			if err != nil {
-				fmt.Println("Error Reading", err.Error())
+		accept_func := func() {
+			fmt.Println("Listening on "+":"+CONN_PORT, unix.Gettid)
+			for i := 0; i < 2; i++ {
+				// Listen for an incoming connection.
+				conn, err := l.Accept()
+				if err != nil {
+					fmt.Println("Error accepting: ", err.Error())
+					os.Exit(1)
+				}
+
+				buf := make([]byte, 1024)
+				_, err = conn.Read(buf)
+				if err != nil {
+					fmt.Println("Error Reading", err.Error())
+				}
+				msg := &Message{conn: conn, name: path}
+				msgc <- msg
 			}
-			conn.Write([]byte("Message received."))
-			conn.Close()
-			fmt.Println("Message on ", path)
-			done <- struct{}{}
+			fmt.Println("Done", path)
 		}
-		fmt.Println("Done", path)
+		go accept_func()
 	}
+
 	go nsfunc("coke")
 	go nsfunc("waqas")
 
 	for i := 0; i < 4; i++ {
 		<-done
 	}
+	close(msgc)
+
 	//	ns, err := GetFromThread()
 	//	if err != nil {
 	//		t.Fatal(err)
